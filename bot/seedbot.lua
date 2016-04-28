459970ff7f014969515b788b29ac2435f6fa7f1a
queenkaiser540 <kaiser540queen@gmail.com>
2016-04-28 18:13:52 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {78916979,0,0,0,0,tonumber(our_id)},--Sudo users
+    sudo_users = {159359772,0,0,0,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
